65a5b9086ecee0237eba1cba372058618bdc51ca
Palin Aleksi <s2001030@edu.bc.fi>
2022-03-03 13:02:52 +0200

    Changes to givecardspositions script

--- Assets/Scripts/GiveCardsPositions.cs
+++ Assets/Scripts/GiveCardsPositions.cs
@@ -12,30 +12,40 @@ public class GiveCardsPositions : MonoBehaviour
     //Annetaan Maximi boundarit eli reunat minkä yli ei voi mennä ja scripti laittaa kortit tasaisin margini välein halutulle välille korttien mittojen perusteella. (Tähän tarvitaan kortin x & y mitat) (maximi boundarit koskee myös Y akselia)
     //scriptille myös annetaan minkä kokoiseksi arvaus alusta halutaan mm. 4x4, 8x8, 16x16 yms. jolloin se asettaa kortita oikein (Miten? en tiedä googlaa)
 
-    [Header("Boundary Values")]
-    public BoxCollider2D Bounder;
-
-    private float BoundX;
-    private float BoundY;
-
-    [Header("Card size example")]
-    public GameObject CardSample;
-
-    private float CardSizeX;
-    private float CardSizeY;
-
-    [Header("Grid size")]
-    public int SizeX;
-    public int SizeY;
+    [SerializeField]
+    private int rows = 5;
+    [SerializeField]
+    private int cols = 8;
+    [SerializeField]
+    private float tileSize = 1;
 
     private void Start()
     {
-        BoundX = Bounder.size.x;
-        BoundY = Bounder.size.y;
-
-        CardSizeX = CardSample.GetComponent<BoxCollider2D>().size.x;
-        CardSizeY = CardSample.GetComponent<BoxCollider2D>().size.y;
+        GenerateGrid();
     }
 
+    private void GenerateGrid()
+    {
+        GameObject ReferenceTile = (GameObject)Instantiate(Resources.Load("SampleCard"));
+        for (int  row = 0;  row < rows;  row++)
+        {
+            for (int col = 0; col < cols; col++)
+            {
+                GameObject tile = (GameObject)Instantiate(ReferenceTile, transform);
+
+                float posX = col * tileSize;
+                float posY = row * -tileSize;
+
+                tile.transform.position = new Vector2(posX, posY);
+            }
+        }
+        Destroy(ReferenceTile);
+
+        float gridW = cols * tileSize;
+        float gridH = rows * tileSize;
+
+        transform.position = new Vector2(gridW / 2 + tileSize / 2, gridH / 2 - tileSize / 2);
+
+    }
 
 }
