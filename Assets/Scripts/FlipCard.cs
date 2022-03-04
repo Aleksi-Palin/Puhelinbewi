0fcbf3ca75592d15c9bb8c22a43c3cc93397914f
Aleksi-Palin <Aleksi.palin@gmail.com>
2022-03-04 14:17:52 +0200

    Attempt on checking if is same card

--- Assets/Scripts/FlipCard.cs
+++ Assets/Scripts/FlipCard.cs
@@ -6,4 +6,28 @@ public class FlipCard : MonoBehaviour
 {
     //t‰m‰ skripti k‰‰nt‰‰ kortin ymp‰ri kun korttia klikataan
     //t‰m‰n pit‰‰ teh‰ yhteistyˆt‰ playercontrollerin kanssa
+
+    private GameManager gm;
+
+    private void Start()
+    {
+        gm = FindObjectOfType<GameManager>();
+    }
+
+    private void OnMouseDown()
+    {
+        
+        if (gm.FirstCard == string.Empty)
+        {
+            gm.FirstCard = this.gameObject.name;
+
+            gm.FirstCardIsFlipped = true;
+            print("wat");
+        }
+        else
+        {
+            gm.SecondCard = gameObject.name;
+        }
+        gm.CheckForCombo();
+    }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,5 +7,69 @@ public class GameManager : MonoBehaviour
     //t‰ss‰ scriptiss‰ checkataan jos 2 samaa korttia on k‰‰nnetty oikein p‰in ja jos on niin ne j‰‰ oikein p‰in
     //t‰m‰ tehd‰‰n tsekkaamlla klikatun kortin nimi ja jos tietyss‰ aika ikkunassa saman niminen kortti klikataan myˆs ne j‰‰ oikein p‰in
 
-    
+    [Header("Timer")]
+    [SerializeField]
+    private float WaitTime = 2.0f;
+
+    [Header("Booleans")]
+    [SerializeField]
+    public bool FirstCardIsFlipped;
+
+    [Header("Flipped Cards")]
+    public string FirstCard;
+    public string SecondCard;
+
+    [Header("Instance")]
+    private static GameManager Instance;
+
+    private void Start()
+    {
+        Instance = FindObjectOfType<GameManager>();
+
+        if(Instance != null)
+        {
+            return;
+        }
+
+        Instance = this;
+
+        
+        WaitTime = 2f;
+    }
+
+    private void Update()
+    {
+        //print(FirstCard + " " + SecondCard);
+    }
+
+
+    //katotaan ett‰ jos ensimm‰inen kortti on k‰‰nnetty odotettaan 2 sekunttia toista korttia varten ja katotaan onko ne sama kortti ja jos ei k‰‰nnet‰‰n ne takaisin jos on samna niin j‰‰ oikein p‰in.
+    public void CheckForCombo()
+    {
+        //print("before loop");
+        while (FirstCardIsFlipped)
+        {
+            if (FirstCard == SecondCard)
+            {
+                print("found combo");
+            }
+
+
+            if (WaitTime > 0)
+            {
+                
+                WaitTime -= Time.deltaTime;
+            }
+            else
+            {
+                
+                //WaitTime = 2f;
+                FirstCardIsFlipped = false;
+                
+            }
+
+            
+        }
+        
+    }
 }
